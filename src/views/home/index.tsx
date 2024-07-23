08d3bab6bb1702dd6794b7b5c1c967c661fc1442
zf <zf18600004319@aliyun.com>
2024-07-23 17:58:50 +0800

    添加菜单

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,50 @@
 import "./style.scss"
-import {memo} from 'react'
+import {memo, useState} from 'react'
 import {withRouter} from "@/hoc"
+import { Menu } from 'antd';
+import type { MenuProps } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 const Index = memo((props: { router: any }) => {
+
+  const [collapsed, setCollapsed] = useState(true);
+  const items: MenuItem[] = [
+    {
+      key: 'sub1',
+      label: '好友',
+      icon: <MailOutlined/>,
+    },
+    {
+      key: 'sub1',
+      label: '群聊',
+      icon: <MailOutlined/>,
+    },
+  ]
+
+
+
+
 
 
   return (
     <div className="zf-home-page">
-      首页
+
+
+      <Menu
+        inlineCollapsed={collapsed}
+        onClick={() => {
+          setCollapsed(!collapsed);
+        }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        items={items}
+      />
+
+
     </div>
   );
 });
